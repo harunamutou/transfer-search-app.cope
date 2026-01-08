efed1f87f6cf9b68370119267167f383ffddd5f0
harunamutou <dd51ef200@gmail.com>
2026-01-09 03:42:50 +0900

    Delete functions directory

--- functions/resetStations.js
+++
@@ -1,4 +0,0 @@
-export async function onRequestPost({ env }) {
-  await env.DB.prepare(`DELETE FROM stations`).run();
-  return new Response(JSON.stringify({ message: "駅データを完全リセットしました" }), { status: 200 });
-}
